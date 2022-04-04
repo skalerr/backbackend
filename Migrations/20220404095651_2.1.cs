9d433193b2a32e3b2f2ece2b79861843bad170dc
Skalerr <rob06121@gmail.com>
2022-04-04 14:57:20 +0500

    2.2

---
+++ Migrations/20220404095651_2.1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace rpg2.Migrations
+{
+    public partial class _21 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "vmodelsumm",
+                table: "UserData",
+                newName: "VmodelSumm");
+
+            migrationBuilder.RenameColumn(
+                name: "vmodelday",
+                table: "UserData",
+                newName: "VmodelDay");
+
+            migrationBuilder.RenameColumn(
+                name: "inputtel",
+                table: "UserData",
+                newName: "InputTel");
+
+            migrationBuilder.RenameColumn(
+                name: "inputname",
+                table: "UserData",
+                newName: "InputName");
+
+            migrationBuilder.RenameColumn(
+                name: "inputemail",
+                table: "UserData",
+                newName: "InputEmail");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "VmodelSumm",
+                table: "UserData",
+                newName: "vmodelsumm");
+
+            migrationBuilder.RenameColumn(
+                name: "VmodelDay",
+                table: "UserData",
+                newName: "vmodelday");
+
+            migrationBuilder.RenameColumn(
+                name: "InputTel",
+                table: "UserData",
+                newName: "inputtel");
+
+            migrationBuilder.RenameColumn(
+                name: "InputName",
+                table: "UserData",
+                newName: "inputname");
+
+            migrationBuilder.RenameColumn(
+                name: "InputEmail",
+                table: "UserData",
+                newName: "inputemail");
+        }
+    }
+}
